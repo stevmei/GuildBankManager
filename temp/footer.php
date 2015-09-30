fc59ce4bde1cb06485ada1bfa72761d5de43efda
Steven Meisberger <admin@sigmaroot.de>
2015-10-01 00:13:05 +0200

    v_3_2
    UPDATE 01.10.2015

--- temp/footer.php
+++ temp/footer.php
@@ -1,7 +1,7 @@
 
 <!-- FOOTER.PHP -->
 <div class="footerCopyright">
-GuildBankManager v3.0 Web-Interface | &copy; 2015 Sigmaroot.de | <a href="http://www.sigmaroot.de/" target="_blank">Sigmaroot Homepage</a>
+GuildBankManager v3.2 Web-Interface | &copy; 2015 Sigmaroot.de | <a href="http://www.sigmaroot.de/" target="_blank">Sigmaroot Homepage</a>
 </div>
 </div>
 </body>
